99238ed46153bce0d7954d62878c6630cf71a24c
satya.silaparasetti <satya.silaparasetti@synectiks.com>
2024-01-08 11:50:37 -0500

    initial commit for eks and appkube deployment

--- README.md
+++ README.md
@@ -1,14 +1,39 @@
-# Welcome to your CDK TypeScript project
+# Welcome to appkube product deployment
 
-This is a blank project for CDK development with TypeScript.
+# prerequisities
+To use the eks-blueprints module, you must have node.js, npm, aws, aws-cdk(version as per package.json)
+* `Install node, npm, make, istioctl(1.20.1) and kubectl`
+* `aws --version`
+* `npm install -g aws-cdk@2.115.0`
+* `cdk --version`
+* `cdk init app --language typescript`
 
-The `cdk.json` file tells the CDK Toolkit how to execute your app.
+# EKS deployment
+target account and region must be bootstrapped prior to deploying stacks
+* `cdk bootstrap aws://<AWS-account-number>/<region-to-bootstrap>`
+* `git clone <this repo>`
+* `cdk deploy ak-qa-eks`
+After deployment configure kubebctl. Below is a sample command, actual is part of 'cdk deploy' output
+* `aws eks update-kubeconfig --name ak-qa-eks --region us-east-2 --role-arn arn:aws:iam::657907747545:role/ak-qa-eks-AdminRole38563C57-rfo013EkdNlw`
 
-## Useful commands
+# Post cluster deployment
+ * ``
+ * `kubectl apply -f components/istio-addons/`
+ * `istioctl install -f components/istio-ingress-egress.yaml`
+ * Add route 53 entry for nlb created above
 
-* `npm run build`   compile typescript to js
-* `npm run watch`   watch for changes and compile
-* `npm run test`    perform the jest unit tests
-* `npx cdk deploy`  deploy this stack to your default AWS account/region
-* `npx cdk diff`    compare deployed stack with current state
-* `npx cdk synth`   emits the synthesized CloudFormation template
+
+# cmdb service deployment
+ 
+ * check README from cmdb deployment repo
+
+# Cleanup
+ *  Uninstall cmdb service - check README from cmdb deployment repo
+ * `kubectl delete -f components/appkube-api-istiogw.yaml`
+ * `kubectl delete -f components/istio-ingress-egress.yaml`
+ * `kubectl delete -f components/istio-addons/`
+ * `istioctl operator remove --purge -n istio-system` confirmation prompt should be removed
+ * `istioctl uninstall --purge` # load balaner won't be deleted
+ * `kubectl delete namespace istio-system`
+ * `cdk destroy ak-qa-eks`
+cleanup route53 entry
--- bin/learning-eks-blueprint.ts
+++ bin/learning-eks-blueprint.ts
@@ -1,21 +1,95 @@
-#!/usr/bin/env node
-import 'source-map-support/register';
 import * as cdk from 'aws-cdk-lib';
-import { LearningEksBlueprintStack } from '../lib/learning-eks-blueprint-stack';
+import * as blueprints from '@aws-quickstart/eks-blueprints';
+// import { KubernetesVersion, NodegroupAmiType } from 'aws-cdk-lib/aws-eks';
+// import { CfnIPAMScope } from 'aws-cdk-lib/aws-ec2';
+// import * as iam from '@aws-cdk-lib/aws-iam';
+// import * as ec2 from '@aws-cdk-lib/aws-ec2';
+
+const PROJ = 'ak'; // appkube
+const ENV = 'qa';
+const CLUSTER_NAME = PROJ.concat('-',ENV,'-','eks')
+const TAG = PROJ.concat('-',ENV)
+console.log("EKS Cluster name: " + CLUSTER_NAME)
 
 const app = new cdk.App();
-new LearningEksBlueprintStack(app, 'LearningEksBlueprintStack', {
-  /* If you don't specify 'env', this stack will be environment-agnostic.
-   * Account/Region-dependent features and context lookups will not work,
-   * but a single synthesized template can be deployed anywhere. */
+const account = '657907747545';
+const region = 'us-east-2';
+const version = 'auto'
 
-  /* Uncomment the next line to specialize this stack for the AWS Account
-   * and Region that are implied by the current CLI configuration. */
-  // env: { account: process.env.CDK_DEFAULT_ACCOUNT, region: process.env.CDK_DEFAULT_REGION },
+const addOns: Array<blueprints.ClusterAddOn> = [
+    new blueprints.addons.ArgoCDAddOn(),
+    // new blueprints.addons.CalicoOperatorAddOn(),
+    new blueprints.addons.MetricsServerAddOn(),
+    new blueprints.addons.ClusterAutoScalerAddOn(),
+    new blueprints.addons.AwsLoadBalancerControllerAddOn(),
+    new blueprints.addons.VpcCniAddOn(),
+    new blueprints.addons.EbsCsiDriverAddOn(),
+    new blueprints.addons.CoreDnsAddOn(),
+    new blueprints.addons.KubeProxyAddOn(),
+    new blueprints.addons.IstioBaseAddOn(),
+    new blueprints.addons.IstioControlPlaneAddOn(),
+    // new blueprints.addons.PrometheusNodeExporterAddOn(),
+    // new blueprints.addons.GrafanaOperatorAddon()
+    // new blueprints.addons.CloudWatchAdotAddOn()
+];
 
-  /* Uncomment the next line if you know exactly what Account and Region you
-   * want to deploy the stack to. */
-  // env: { account: '123456789012', region: 'us-east-1' },
+// const vpc = new cdk.aws_ec2.Vpc(app, TAG.concat('-', 'vpc'), {
+//     cidr: '10.0.0.0/16',
+//     maxAzs: 2,
+//     subnetConfiguration: [
+//       {
+//         cidrMask: 16,
+//         name: 'public',
+//         subnetType: cdk.aws_ec2.SubnetType.PUBLIC
+//       },
+//       {
+//         cidrMask: 16,
+//         name: 'private',
+//         subnetType: cdk.aws_ec2.SubnetType.PRIVATE_WITH_EGRESS
+//       },
+//     ],
+// });
 
-  /* For more information, see https://docs.aws.amazon.com/cdk/latest/guide/environments.html */
+const clusterProvider = new blueprints.GenericClusterProvider({
+    version: cdk.aws_eks.KubernetesVersion.V1_27,
+    // vpc,
+    tags: {
+        "Name": TAG.concat('-','cluster'),
+        "Type": "generic-cluster",
+        "Project": TAG
+    },
+    serviceIpv4Cidr: "10.43.0.0/16",
+    // if needed use this to register an auth role integrate with RBAC
+    mastersRole: blueprints.getResource(context => {
+        return new cdk.aws_iam.Role(context.scope, 'AdminRole', { assumedBy: new cdk.aws_iam.AccountRootPrincipal() });
+    }),
+    managedNodeGroups: [
+        {
+            id: "mng1",
+            amiType: cdk.aws_eks.NodegroupAmiType.AL2_X86_64,
+            instanceTypes: [new cdk.aws_ec2.InstanceType('m5.2xlarge')],
+            desiredSize: 2,
+            maxSize: 3, 
+            nodeGroupSubnets: { subnetType: cdk.aws_ec2.SubnetType.PRIVATE_WITH_EGRESS },
+            launchTemplate: {
+                // You can pass Custom Tags to Launch Templates which gets propagated to worker nodes.
+                tags: {
+                    "Project": TAG,
+                    "Name": TAG.concat('-', 'NodeGroup'),
+                    "Type": "Managed-Node-Group",
+                    "LaunchTemplate": "Custom",
+                    "Instance": "ONDEMAND"
+                }
+            }
+        }
+    ]
 });
+
+const stack = blueprints.EksBlueprint.builder()
+    .account(account)
+    .region(region)
+    .version(version)
+    .addOns(...addOns)
+    .clusterProvider(clusterProvider)
+    .useDefaultSecretEncryption(true) // set to false to turn secret encryption off (non-production/demo cases)
+    .build(app, CLUSTER_NAME);
